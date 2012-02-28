68b6a6f3defee92d432a25a5679eb974601e06a3
Alex Chrome <dify.chrome@gmail.com>
2012-02-28 13:31:08 +0700

    added a few more journal entries related to subtasks

--- lib/redmine_advanced_issue_history/hooks/controller_issues_new_after_save_hook.rb
+++ lib/redmine_advanced_issue_history/hooks/controller_issues_new_after_save_hook.rb
@@ -5,14 +5,34 @@ module RedmineAdvancedIssueHistory
       # * :issue => Issue being saved
       # * :params => HTML parameters
       #
+
+      def controller_issues_edit_before_save(context={})
+        issue = context[:issue]
+        issue_params = context[:params][:issue]
+
+        if issue.parent.present? && (issue.parent.id != issue_params[:parent_issue_id])
+          note = "Sub task '#{issue}' has been removed"
+          journal = Journal.new(:journalized => issue.parent, :user => User.current, :notes => note, :is_system_note => true)
+          journal.save
+        end
+        if issue_params[:parent_issue_id].present? && (issue.parent.try(:id) != issue_params[:parent_issue_id])
+          note = "Sub task '#{issue}' was added"
+          journal = Journal.new(:journalized => Issue.find(issue_params[:parent_issue_id]), :user => User.current, :notes => note, :is_system_note => true)
+          journal.save
+        end
+      end
+
       def controller_issues_new_after_save(context={})
         issue = context[:issue]
         unless issue.parent.nil?
           parent_issue = issue.parent
           user = User.current
-          note = "Sub tast '#{issue}' was added"
-          journal = Journal.new(:journalized => parent_issue, :user => user, :notes => note, :is_system_note=> true)
-          journal.save
+          parent_task_note = "Sub task '#{issue}' was added"
+          sub_task_note = "Has been added as sub task to '#{parent_issue}'"
+
+          parent_task_journal  = Journal.new(:journalized => parent_issue, :user => user, :notes => parent_task_note, :is_system_note=> true)
+          sub_task_journal = Journal.new(:journalized => issue, :user => user, :notes => sub_task_note, :is_system_note=> true)
+          parent_task_journal.save && sub_task_journal.save
         end
       end
     end
