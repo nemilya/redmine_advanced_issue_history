c2058fd78c6b353dd672b637523860e9518d25ec
Ilya Nemihin <nemilya@gmail.com>
2012-11-29 12:27:49 +0600

    compatible with 1.4.4 (1.4.x) redmine version

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_advanced_issue_history do
   name 'Redmine Advanced Issue History plugin'
   author 'Ilya Nemihin'
   description 'New events store in Issue history'
-  version '0.0.9'
+  version '0.1'
   url 'https://github.com/nemilya/redmine_advanced_issue_history'
   author_url ''
 end
@@ -14,7 +14,13 @@ end
 
 require 'dispatcher'
 Dispatcher.to_prepare :redmine_advanced_issue_history do
-  require_dependency 'issue_relations_controller'
+  if Redmine::VERSION.to_s >= '1.4.0'
+    # tested for 1.4.4
+    require 'redmine_advanced_issue_history/patches/issue_relations_controller_patch_1_4'
+  else
+    # redmine 1.2.x
+    require 'redmine_advanced_issue_history/patches/issue_relations_controller_patch'
+  end
   IssueRelationsController.send(:include, RedmineAdvancedIssueHistory::Patches::IssueRelationsControllerPatch)
 
   require_dependency 'watcher'
--- lib/redmine_advanced_issue_history/patches/issue_relations_controller_patch_1_4.rb
+++ lib/redmine_advanced_issue_history/patches/issue_relations_controller_patch_1_4.rb
@@ -1,4 +1,3 @@
-# tested for Redmine 1.2
 module RedmineAdvancedIssueHistory
   module Patches
     module IssueRelationsControllerPatch
@@ -7,7 +6,7 @@ module RedmineAdvancedIssueHistory
 
         base.send(:include, InstanceMethods)
         base.class_eval do
-          alias_method_chain :new,     :update_history
+          alias_method_chain :create,  :update_history
           alias_method_chain :destroy, :update_history
           unloadable
           helper :journals
@@ -22,13 +21,14 @@ module RedmineAdvancedIssueHistory
 
       module InstanceMethods
 
-        def new_with_update_history
+        def create_with_update_history
+          # based on redmine 1.4.4
           @relation = IssueRelation.new(params[:relation])
           @relation.issue_from = @issue
-          if params[:relation] && m = params[:relation][:issue_to_id].to_s.match(/^#?(\d+)$/)
+          if params[:relation] && m = params[:relation][:issue_to_id].to_s.strip.match(/^#?(\d+)$/)
             @relation.issue_to = Issue.visible.find_by_id(m[1].to_i)
           end
-          @relation.save if request.post?
+          saved = @relation.save
 
           # ilya
           if @relation.errors.empty? && request.post?
@@ -48,54 +48,63 @@ module RedmineAdvancedIssueHistory
               @relations = @issue.relations.select {|r| r.other_issue(@issue) && r.other_issue(@issue).visible? }
               render :update do |page|
                 page.replace_html "relations", :partial => 'issues/relations'
+
                 # ilya
                 @journals = @issue.journals.find(:all, :include => [:user, :details], :order => "#{Journal.table_name}.created_on ASC")
                 @journals.each_with_index {|j,i| j.indice = i+1}
                 @journals.reverse! if User.current.wants_comments_in_reverse_order?
                 page.replace_html "history", :partial => 'issues/history', :locals => { :issue => @issue, :journals => @journals }
                 # /ilya
+
                 if @relation.errors.empty?
                   page << "$('relation_delay').value = ''"
                   page << "$('relation_issue_to_id').value = ''"
                 end
               end
             end
+            format.api {
+              if saved
+                render :action => 'show', :status => :created, :location => relation_url(@relation)
+              else
+                render_validation_errors(@relation)
+              end
+            }
           end
         end
 
 
         def destroy_with_update_history
-          relation = IssueRelation.find(params[:id])
-          if request.post? && @issue.relations.include?(relation)
-            relation.destroy
+          # based on redmine 1.4.4
 
-            # ilya
-            note = "Relation '#{relation.type}' to '#{relation.issue_to}' was destroyed"
-            journal = Journal.new(:journalized => relation.issue_from, :user => User.current, :notes => note, :is_system_note=> true)
-            journal.save
+          raise Unauthorized unless @relation.deletable?
+          @relation.destroy
 
-            note = "Relation '#{relation.type}' to '#{relation.issue_from}' was destroyed"
-            journal = Journal.new(:journalized => relation.issue_to, :user => User.current, :notes => note, :is_system_note=> true)
-            journal.save
-            # /ilya
+          # ilya
+          note = "Relation '#{@relation.type}' to '#{@relation.issue_to}' was destroyed"
+          journal = Journal.new(:journalized => @relation.issue_from, :user => User.current, :notes => note, :is_system_note=> true)
+          journal.save
 
-            @issue.reload
-          end
+          note = "Relation '#{@relation.type}' to '#{@relation.issue_from}' was destroyed"
+          journal = Journal.new(:journalized => @relation.issue_to, :user => User.current, :notes => note, :is_system_note=> true)
+          journal.save
+          # /ilya
 
           respond_to do |format|
-            format.html { redirect_to :controller => 'issues', :action => 'show', :id => @issue }
-            format.js {
-              @relations = @issue.relations.select {|r| r.other_issue(@issue) && r.other_issue(@issue).visible? }
-              render(:update) do |page| 
+            format.html { redirect_to issue_path } # TODO : does this really work since @issue is always nil? What is it useful to?
+            format.js   { 
+              render(:update) { |page|
+
                 # ilya
                 @journals = @issue.journals.find(:all, :include => [:user, :details], :order => "#{Journal.table_name}.created_on ASC")
                 @journals.each_with_index {|j,i| j.indice = i+1}
                 @journals.reverse! if User.current.wants_comments_in_reverse_order?
                 page.replace_html "history", :partial => 'issues/history', :locals => { :issue => @issue, :journals => @journals }
                 # /ilya
-                page.replace_html "relations", :partial => 'issues/relations'
-              end
+                
+                page.remove "relation-#{@relation.id}"
+              }
             }
+            format.api  { head :ok }
           end
         end
 
